a5e99ba7db3b0c1745a0fa209a8e6c105092bf08
D-Sketon <2055272094@qq.com>
2022-12-07 00:02:41 +0800

    feat: 添加设置Activity

--- app/src/main/java/com/example/guyunwu/MainActivity.java
+++ app/src/main/java/com/example/guyunwu/MainActivity.java
@@ -1,24 +1,31 @@
 package com.example.guyunwu;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import com.example.guyunwu.databinding.ActivityMainBinding;
+import com.example.guyunwu.ui.explore.daily.DailySentenceActivity;
+import com.example.guyunwu.ui.user.setting.SettingActivity;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import static androidx.navigation.ui.NavigationUI.onNavDestinationSelected;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
 
-    private Menu mMenu;
+    private Menu menu;
 
+    private int currentFragment;
+
+    @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,30 +34,59 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+        // 重写导航栏监听事件用于改变ActionBar
+        navView.setOnItemSelectedListener(item -> {
+            this.currentFragment = item.getItemId();
+            onPrepareOptionsMenu(menu);
+            return onNavDestinationSelected(item, navController);
+        });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_layout, menu);
-        this.mMenu = menu;
+        this.menu = menu;
         return true;
     }
 
-    // 给菜单按钮添加点击事件
+    @SuppressLint({"NonConstantResourceId", "ResourceType"})
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        super.onPrepareOptionsMenu(menu);
+        menu.clear();
+        switch (this.currentFragment) {
+            case R.id.navigation_dashboard:
+                break;
+            case R.id.navigation_notifications:
+                MenuItem setting = menu.add(Menu.NONE, R.drawable.ic_user_setting_24dp, 1, "");
+                setting.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+                setting.setIcon(R.drawable.ic_user_setting_24dp);
+                break;
+            case R.id.navigation_home:
+
+                break;
+        }
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+
+    @SuppressLint("NonConstantResourceId")
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getItemId() ==  R.id.menu_layout) {
-            System.out.println("111");
+        switch (item.getItemId()) {
+            case R.drawable.ic_user_setting_24dp:
+                Intent settingPage = new Intent();
+                settingPage.setClass(this, SettingActivity.class);
+                startActivity(settingPage);
+                break;
         }
         return false;
     }
-
 }
---
+++ app/src/main/java/com/example/guyunwu/ui/user/setting/SettingActivity.java
@@ -0,0 +1,34 @@
+package com.example.guyunwu.ui.user.setting;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.guyunwu.R;
+
+public class SettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+        initActionBar();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {   //返回键的id
+            this.finish();
+            return false;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void initActionBar() {
+        ActionBar bar = getSupportActionBar();
+        if (bar != null) {
+            bar.setTitle("设置");
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+}
