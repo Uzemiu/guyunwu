5d046d76207e9b322d0ecf4346bad8bf9b96086c
re20051 <2698471674@qq.com>
2022-12-06 21:12:01 +0800

    首页

--- app/src/main/java/com/example/guyunwu/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/guyunwu/ui/home/HomeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -22,8 +23,6 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
