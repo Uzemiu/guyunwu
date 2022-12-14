b36a195dadbb8504c02f69bf645c584f00fa0a97
D-Sketon <2055272094@qq.com>
2022-12-14 15:37:00 +0800

    feat: 打卡接口对接

--- app/src/main/java/com/example/guyunwu/api/LearnRequest.java
+++ app/src/main/java/com/example/guyunwu/api/LearnRequest.java
@@ -8,6 +8,7 @@ import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
 
 import java.util.Date;
 import java.util.List;
@@ -31,4 +32,7 @@ public interface LearnRequest {
 
     @POST("/learn/learn")
     Call<BaseResponse<Object>> learn(@Body Long wordId);
+
+    @PUT("/learn/clockIn")
+    Call<BaseResponse<Object>> clockIn();
 }
--- app/src/main/java/com/example/guyunwu/ui/home/signIn/SignInActivity.java
+++ app/src/main/java/com/example/guyunwu/ui/home/signIn/SignInActivity.java
@@ -1,25 +1,57 @@
 package com.example.guyunwu.ui.home.signIn;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
-import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.guyunwu.R;
+import com.example.guyunwu.api.BaseResponse;
+import com.example.guyunwu.api.LearnRequest;
+import com.example.guyunwu.api.RequestModule;
 import org.xutils.x;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 import java.time.LocalDateTime;
 
 public class SignInActivity extends AppCompatActivity {
 
+    private static final String TAG = "SignInActivity";
     private String imageUrl;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
         initActionBar();
         initDate();
+        trySignIn();
+    }
+
+    private void trySignIn() {
+        LearnRequest learnRequest = RequestModule.LEARN_REQUEST;
+
+        learnRequest.clockIn().enqueue(new Callback<BaseResponse<Object>>() {
+            @Override
+            public void onResponse(Call<BaseResponse<Object>> call, Response<BaseResponse<Object>> response) {
+                BaseResponse<Object> body = response.body();
+                if (body == null || body.getCode() != 200) {
+                    onFailure(call, new Throwable("打卡失败"));
+                } else {
+                    Toast.makeText(SignInActivity.this, "打卡成功", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<BaseResponse<Object>> call, Throwable t) {
+                Toast.makeText(SignInActivity.this, t.getMessage(), Toast.LENGTH_SHORT).show();
+                Log.e(TAG, "onFailure: ", t);
+            }
+        });
     }
 
     private void initDate() {
